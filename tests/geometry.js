c20a8d799e0e9e0a746991bcebe5045830c92353
Miroslav Nedyalkov <miroslav.nedialkov@gmail.com>
2013-09-14 19:30:42 +0300

    Some tests added

--- tests/geometry.js
+++ tests/geometry.js
@@ -1,9 +1,11 @@
+QUnit.module("Point.parse");
+
 test("Parse point with comma.", function() {
 	var g = require('geometry');
 	
 	var parsed = g.Point.parse("1,2");
 	
-    deepEqual(new g.Point(1, 2), parsed)
+    deepEqual(parsed, new g.Point(1, 2))
 });
 
 test("Parse point with space.", function() {
@@ -11,7 +13,7 @@ test("Parse point with space.", function() {
 	
 	var parsed = g.Point.parse("1 2");
 	
-    deepEqual(new g.Point(1, 2), parsed)
+    deepEqual(parsed, new g.Point(1, 2))
 });
 
 test("Parse point with space and comma.", function() {
@@ -19,5 +21,21 @@ test("Parse point with space and comma.", function() {
 	
 	var parsed = g.Point.parse("1, 2");
 	
-    deepEqual(new g.Point(1, 2), parsed)
+    deepEqual(parsed, new g.Point(1, 2))
+});
+
+QUnit.module("Polygon.parse");
+
+test("Single point.", function() {
+	var g = require('geometry');
+	
+	var parsed = Polygon.parse("1,2");
+    deepEqual(parsed, new Polygon([new g.Point(1, 2)]))
+});
+
+test("Multiple points.", function() {
+	var g = require('geometry');
+	
+	var parsed = Polygon.parse("1,2 10,10 0,0");
+    deepEqual(parsed, new Polygon([new g.Point(1, 2), new g.Point(10, 10), new g.Point(0, 0)]))
 });
