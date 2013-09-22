9b45220088918a97424dd2e9be90fea99d8fb464
miromiroslavov <miroslav.miroslavov@gmail.com>
2013-09-22 08:04:55 +0300

    Editor imprrovements

--- models.js
+++ models.js
@@ -1,147 +1,129 @@
-var MongoClient = require('mongodb').MongoClient;
-var format = require('util').format;
-
-var Class = require('./modules/oop').Class;
-
-function printMongoCollection() {
-  MongoClient.connect('mongodb://intelli-place:Asdf1234@ds041168.mongolab.com:41168/intelli-place', function (err, db) {
-    if (err) throw err;
-
-		var collection = db.collection('workspaces');
-		  // Locate all the entries using find
-		collection.findOne({'_id': 'miromiroslavov'}, function(err, result) {
-			console.log(result);
-			// Let's close the db
-			db.close();		
-		});
-	});
-    var collection = db.collection('workspaces');
-    // Locate all the entries using find
-    collection.findOne({'_id': 'miroslav.miroslavov@gmail.com'}, function (err, result) {
-      console.log(result);
-      // Let's close the db
-      db.close();
-    });
-  });
-		var collection = db.collection('workspaces');
-        // Locate all the entries using find
-		collection.findOne({'_id': 'miromiroslavov'}, function(err, result) {
-			console.log(result);
-			// Let's close the db
-			db.close();		
-		});
-	});
-}
-
-exports.printMongoCollection = printMongoCollection
-
-exports.getWorkspaces = function (callback) {
-  MongoClient.connect('mongodb://intelli-place:Asdf1234@ds041168.mongolab.com:41168/intelli-place', function (err, db) {
-    if (err) throw err;
-
-    var collection = db.collection('workspaces');
-    // Locate all the entries using find
-    collection.find().toArray(function (err, items) {
-      if (err) throw err;
-
-      callback(items);
-      // Let's close the db
-      db.close();
-    });
-  });
-}
-
-var createWorkspace = function(json) {
-	var result = new Object();
-	result.name = json.name;
-	result.offices = [];
-	result.toString = function() {
-		return result.name + ', []';
-	}
-	return result;
-var createWorkspace = function (json) {
-  var result = new Object();
-  result.name = json.name;
-  result.offices = [];
-  result.toString = function () {
-    return result.name + ', []';
-  }
-  return result;
-var createWorkspace = function(json) {
-	return Class.extend({
-        name: json.name,
-        offices: [],
-        toString: function() {
-            return this.name + ', []';
-        }
-	});
-}
-
-var workspace = {
-  "_id": "miroslav.miroslavov@gmail.com",
-  "name": "Sofia Valley",
-  "offices": [
-    {
-      "name": "HQ1",
-      "rooms": [
-        {
-          "name": "101",
-          "boundary": "m0,0 L0,500 L500,500 L500,0z",
-          "position": "100 100",
-          "artifacts": [
-            {
-              "type": "air-conditioner",
-              "position": "50 50",
-              "boundary": "m0,0 L0,50 L50,50 L50,0z",
-              "obstacle": false
-            },
-            {
-              "type": "column",
-              "position": "300 150",
-              "boundary": "m0,0 L0,50 L50,50 L50,0z",
-              "obstacle": true
-            }
-          ],
-          "arrangements": [
-            {
-              "name": "plan1",
-              "objects": [
-                {
-                  "type": "desk",
-                  "name": "Miro's desk",
-                  "position": "400 250",
-                  "boundary": "m0,0 L0,50 L50,50 L50,0z"
-                },
-                {
-                  "type": "chair",
-                  "name": "Miro's chair",
-                  "position": "412.5 300",
-                  "boundary": "m0,0 L0,25 L25,25 L25,0z"
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  ]
-};
-
-var office = workspace.offices[0];
-
-var room = office.rooms[0];
-
-console.log(room);
-
-var ext = require('./modules/geometryExtensions');
-var g = require('geometry');
-var p = new ext.Polygon([new g.Point(0, 0), new g.Point(0, 10), new g.Point(10, 10), new g.Point(10, 0)]);
-console.log(p);
-console.log(p.perimeter());
-console.log(p.area());
-console.log(g.Point.parse('10.5,20'));
-console.log(ext.Polygon.parse('0,0 10,0 10,10 0,10').area());
-// var parser = require('./svgPathGrammer').pathParser;
-
-// var p = parser.parse('m0,0 L0,500 L500,500 L500,0z');
-// console.dir(p);
+//var MongoClient = require('mongodb').MongoClient;
+//var format = require('util').format;
+//
+//var Class = require('./modules/oop').Class;
+//
+//function printMongoCollection() {
+//  MongoClient.connect('mongodb://intelli-place:Asdf1234@ds041168.mongolab.com:41168/intelli-place', function (err, db) {
+//    if (err) throw err;
+//
+//    var collection = db.collection('workspaces');
+//    // Locate all the entries using find
+//    collection.findOne({'_id': 'miromiroslavov'}, function (err, result) {
+//      console.log(result);
+//      // Let's close the db
+//      db.close();
+//    });
+//  });
+//  var collection = db.collection('workspaces');
+//  // Locate all the entries using find
+//  collection.findOne({'_id': 'miroslav.miroslavov@gmail.com'}, function (err, result) {
+//    console.log(result);
+//    // Let's close the db
+//    db.close();
+//  });
+//};
+//var collection = db.collection('workspaces');
+//// Locate all the entries using find
+//collection.findOne({'_id': 'miromiroslavov'}, function (err, result) {
+//  console.log(result);
+//  // Let's close the db
+//  db.close();
+//});
+//
+//exports.printMongoCollection = printMongoCollection
+//
+//exports.getWorkspaces = function (callback) {
+//  MongoClient.connect('mongodb://intelli-place:Asdf1234@ds041168.mongolab.com:41168/intelli-place', function (err, db) {
+//    if (err) throw err;
+//
+//    var collection = db.collection('workspaces');
+//    // Locate all the entries using find
+//    collection.find().toArray(function (err, items) {
+//      if (err) throw err;
+//
+//      callback(items);
+//      // Let's close the db
+//      db.close();
+//    });
+//  });
+//}
+//
+//var createWorkspace = function (json) {
+//  var result = new Object();
+//  result.name = json.name;
+//  result.offices = [];
+//  result.toString = function () {
+//    return result.name + ', []';
+//  }
+//  return result;
+//}
+//
+//var workspace = {
+//  "_id": "miroslav.miroslavov@gmail.com",
+//  "name": "Sofia Valley",
+//  "offices": [
+//    {
+//      "name": "HQ1",
+//      "rooms": [
+//        {
+//          "name": "101",
+//          "boundary": "m0,0 L0,500 L500,500 L500,0z",
+//          "position": "100 100",
+//          "artifacts": [
+//            {
+//              "type": "air-conditioner",
+//              "position": "50 50",
+//              "boundary": "m0,0 L0,50 L50,50 L50,0z",
+//              "obstacle": false
+//            },
+//            {
+//              "type": "column",
+//              "position": "300 150",
+//              "boundary": "m0,0 L0,50 L50,50 L50,0z",
+//              "obstacle": true
+//            }
+//          ],
+//          "arrangements": [
+//            {
+//              "name": "plan1",
+//              "objects": [
+//                {
+//                  "type": "desk",
+//                  "name": "Miro's desk",
+//                  "position": "400 250",
+//                  "boundary": "m0,0 L0,50 L50,50 L50,0z"
+//                },
+//                {
+//                  "type": "chair",
+//                  "name": "Miro's chair",
+//                  "position": "412.5 300",
+//                  "boundary": "m0,0 L0,25 L25,25 L25,0z"
+//                }
+//              ]
+//            }
+//          ]
+//        }
+//      ]
+//    }
+//  ]
+//};
+//
+//var office = workspace.offices[0];
+//
+//var room = office.rooms[0];
+//
+//console.log(room);
+//
+//var ext = require('./modules/geometryExtensions');
+//var g = require('geometry');
+//var p = new ext.Polygon([new g.Point(0, 0), new g.Point(0, 10), new g.Point(10, 10), new g.Point(10, 0)]);
+//console.log(p);
+//console.log(p.perimeter());
+//console.log(p.area());
+//console.log(g.Point.parse('10.5,20'));
+//console.log(ext.Polygon.parse('0,0 10,0 10,10 0,10').area());
+//// var parser = require('./svgPathGrammer').pathParser;
+//
+//// var p = parser.parse('m0,0 L0,500 L500,500 L500,0z');
+//// console.dir(p);
--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -1,119 +1,141 @@
-var shapesSource = [
-  {
-    name: "Air conditioner",
-    data: "m0,0 L0,50 L50,50 L50,0z",
-    background: "steelblue"
-  },
-  {
-    name: "Column",
-    data: "m0,0 L0,50 L50,50 L50,0z",
-    background: "red"
-  },
-  {
-    name: "Desk",
-    data: "m0,0 L0,50 L100,50 L100,0z",
-    background: "Green"
-  },
-  {
-    name: "Chair",
-    data: "m0,0 L0,25 L25,25 L25,0z",
-    background: "black"
-  }
-];
-
-$(function () {
-  $("#canvas").kendoDiagram();
-  var diagram = $("#canvas").data("kendoDiagram"),
-    kendo = window.kendo,
-    Point = kendo.diagram.Point;
-
-  $("#canvas").kendoDropTarget({
-    drop: function (e) {
-      var item = e.draggable.hint.data("data"),
-        pos = e.draggable.hintOffset,
-        dp = diagram.documentToCanvasPoint(new Point(pos.left, pos.top)),
-        normal = diagram._normalizePointZoom(dp.minus(diagram.pan()));
-
-      diagram.addShape(normal, {"data": item["data"], connectors: []});
-    }
-  });
-
-  var list = $("#toolBox").kendoListView({
-    dataSource: {
-      data: shapesSource
+(function ($) {
+  var shapesSource = [
+    {
+      name: "Air conditioner",
+      data: "m0,0 L0,50 L50,50 L50,0z",
+      background: "steelblue"
     },
-    template: kendo.template($("#template").html())
-    //selectable: "single"
-  });
-
-  list.kendoDraggable({
-    filter: "div[role=option]",
-    hint: function (row) {
-      var svg = row.children("svg"),
-        list = $("#toolBox").data("kendoListView"),
-        item = list.dataSource.getByUid(row.data().uid);
-      svg = svg.clone();
-      svg.data("data", item);
-      return svg;
+    {
+      name: "Column",
+      data: "m0,0 L0,50 L50,50 L50,0z",
+      background: "red"
+    },
+    {
+      name: "Desk",
+      data: "m0,0 L0,50 L100,50 L100,0z",
+      background: "Green"
+    },
+    {
+      name: "Chair",
+      data: "m0,0 L0,25 L25,25 L25,0z",
+      background: "black"
     }
-  });
+  ];
 
-  $("#splitter").kendoSplitter({
-    panes: [
-      { collapsible: true },
-      { collapsible: false, size: "75%" }
-    ]
-  });
+  $(function () {
+    $("#canvas").kendoDiagram();
+    var diagram = $("#canvas").data("kendoDiagram"),
+      kendo = window.kendo,
+      Point = kendo.diagram.Point;
 
-  $("#undoButton").click(function (e) {
-    diagram.undo();
-  });
-  $("#redoButton").click(function (e) {
-    diagram.redo();
-  });
+    $("#canvas").kendoDropTarget({
+      drop: function (e) {
+        var item = e.draggable.hint.data("data"),
+          pos = e.draggable.hintOffset,
+          dp = diagram.documentToCanvasPoint(new Point(pos.left, pos.top)),
+          normal = diagram._normalizePointZoom(dp.minus(diagram.pan()));
 
-  function getPoint(str) {
-    var p = str.split(" ");
-    return new Point(p[0], p[1]);
-  }
-
-  function place(obj, options) {
-    var p = getPoint(obj["position"]);
-    options = kendo.deepExtend({"data": obj["boundary"], "connectors": []}, options);
-    if (obj["type"]) {
-      switch (obj["type"]) {
-        case "desk":
-          options.background = "green";
-          break;
-        case "chair":
-          options.background = "black";
-          break;
+        diagram.addShape(normal, {"data": item["data"], connectors: []});
       }
-    }
-    var s = diagram.addShape(p.plus(roomPos), options);
-    var content = obj["name"] ? obj["name"] : obj["type"];
-    $(s.visual.native).kendoTooltip({
-      content: content,
-      width: 100,
-      position: "top"
     });
-  }
 
-  var roomPos;
-  roomPos = getPoint(room["position"]);
+    var list = $("#toolBox").kendoListView({
+      dataSource: {
+        data: shapesSource
+      },
+      template: kendo.template($("#template").html())
+      //selectable: "single"
+    });
 
-  diagram.addShape(roomPos, {data: room["boundary"], background: "gray", "connectors": []});
+    list.kendoDraggable({
+      filter: "div[role=option]",
+      hint: function (row) {
+        var svg = row.children("svg"),
+          list = $("#toolBox").data("kendoListView"),
+          item = list.dataSource.getByUid(row.data().uid);
+        svg = svg.clone();
+        svg.data("data", item);
+        return svg;
+      }
+    });
 
-  var artefacts = room["artifacts"];
-  for (var i = 0; i < artefacts.length; i++) {
-    var a = artefacts[i];
-    place(a, {background: a["obstacle"] === true ? "red" : "steelblue"})
-  }
+    $("#splitter").kendoSplitter({
+      panes: [
+        { collapsible: true },
+        { collapsible: false, size: "75%" }
+      ]
+    });
 
-  var arrs = room["arrangements"],
-    currentArr = arrs[arrs.length - 1];
+    $("#undoButton").click(function (e) {
+      diagram.undo();
+    });
+    $("#redoButton").click(function (e) {
+      diagram.redo();
+    });
+    $("#bringButton").click(function (e) {
+      diagram.bringToFront();
+    });
+    $("#sendBackButton").click(function (e) {
+      diagram.sendToBack();
+    });
 
-  for (var i = 0; i < currentArr["objects"].length; i++) {
-    place(currentArr["objects"][i]);
-  }
-})
+    var slider = $("#slider").kendoSlider({
+      min: 0.55,
+      value: 1,
+      max: 2,
+      smallStep: .1,
+      largeStep: .2,
+      tickPlacement: "none",
+      change: onZoom,
+      slide: onZoom
+    }).data("kendoSlider");
+
+    function onZoom(e) {
+      diagram.zoom(e.value);
+    }
+
+    function getPoint(str) {
+      var p = str.split(" ");
+      return new Point(p[0], p[1]);
+    }
+
+    function place(obj, options) {
+      var p = getPoint(obj["position"]);
+      options = kendo.deepExtend({"data": obj["boundary"], "connectors": []}, options);
+      if (obj["type"]) {
+        switch (obj["type"]) {
+          case "desk":
+            options.background = "green";
+            break;
+          case "chair":
+            options.background = "black";
+            break;
+        }
+      }
+      var s = diagram.addShape(p.plus(roomPos), options);
+      var content = obj["name"] ? obj["name"] : obj["type"];
+      $(s.visual.native).kendoTooltip({
+        content: content,
+        width: 100,
+        position: "top"
+      });
+    }
+
+    var roomPos = getPoint(room["position"]);
+
+    diagram.addShape(roomPos, {data: room["boundary"], background: "gray", "connectors": []});
+
+    var artefacts = room["artifacts"];
+    for (var i = 0; i < artefacts.length; i++) {
+      var a = artefacts[i];
+      place(a, {background: a["obstacle"] === true ? "red" : "steelblue"})
+    }
+
+    var arrs = room["arrangements"],
+      currentArr = arrs[arrs.length - 1];
+
+    for (var i = 0; i < currentArr["objects"].length; i++) {
+      place(currentArr["objects"][i]);
+    }
+  })
+})(window.kendo.jQuery);
